d6351c42bc4bbee12f572d029fb0806df2854b33
GabrielMouttapa <63340505+GabrielMouttapa@users.noreply.github.com>
2021-04-02 00:01:27 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,13 @@
 
 % Maillage
-nom_maillage = "m1.msh";
-[Nbpt,Nbtri,Coorneu,Refneu,Numtri,Reftri,Nbaretes,Numaretes,Refaretes] = lecture_msh(nom_maillage)
+nom_maillage = "carre_0,1.msh";
+[Nbpt,Nbtri,Coorneu,Refneu,Numtri,Reftri,Nbaretes,Numaretes,Refaretes] = lecture_msh(nom_maillage);
+
+alpha = 2;
+L = 3;
+Nbiter = 10;
+uh0 = zeros(Nbpt, 0);
+BB = iter_B(alpha, L, uh0, Nbpt,Nbtri, Coorneu, Numtri, Refneu, Nbiter);
+affiche(BB,Numtri,Coorneu, "blabla");
+
 
