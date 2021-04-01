86cc7c33bec100bfcdd45a081075901a3236d242
GabrielMouttapa <63340505+GabrielMouttapa@users.noreply.github.com>
2021-04-02 00:01:04 +0200

    Update iter_B.m

--- iter_B.m
+++ iter_B.m
@@ -1,6 +1,6 @@
-function [BB] = iter_B (alpha, L, it, uh0, Nbpt,Nbtri, Coorneu, Numtri, Refneu, Nbiter)
+function [BB] = iter_B (alpha, L, uh0, Nbpt,Nbtri, Coorneu, Numtri, Refneu, Nbiter)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                   Calcul de B^(k-1) itéré Nbiter fois
+%                   Calcul de B^(k-1) itÃ©rÃ© Nbiter fois
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Initialisation
@@ -8,11 +8,11 @@ BB = zeros(Nbpt, 1);
 KK = matrice_rigidite(Nbpt, Nbtri, Coorneu, Numtri, Refneu);
 uhk = uh0;
 
-BB = KK*uhk - (alpha / L^2)* scalaireA();  % Pour k=1
+BB = KK*uhk - (alpha / L^2)* scalaireA(uhk, Nbpt, Nbtri, Numtri, Refneu, Coorneu);  % Pour k=1
 
 for k=1:Nbiter                             % Iteration sur les k
    uhk = KK\BB;
-   BB = KK*uhk - (alpha / L^2)* scalaireA();
+   BB = KK*uhk - (alpha / L^2)* scalaireA(uhk, Nbpt, Nbtri, Numtri, Refneu, Coorneu);
 end
 
 BB = scdTerme(alpha, L, Numtri, Coorneu, Nbpt, Nbtri) + BB;
