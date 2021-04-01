5244170195018311659158fca61f6c1c74c59807
GabrielMouttapa <63340505+GabrielMouttapa@users.noreply.github.com>
2021-04-01 21:55:20 +0200

    Update f.m

--- f.m
+++ f.m
@@ -1,18 +1,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % f.m :
-% calcule de l'évaluation du second membre f
+% calcule de l'Ã©valuation du second membre f
 %
 % SYNOPSIS w = f(v)
 %
-% INPUT - v : vecteur évalué par f
+% INPUT - v : vecteur Ã©valuÃ© par f
 %
-% OUTPUT - w : résultat sous forme du flotant
+% OUTPUT - w : rÃ©sultat sous forme du flotant
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function w = f(v)
     x = v(1) ; y = v(2) ;
     z = 1 + pi^2 * cos(pi*x)^2 * sin(pi*y)^2 + pi^2 * sin(pi*x)^2 * cos(pi*y)^2 ;
-    w = 2 * pi^2 * sin(pi*x) * cos(pi*y) * (2+z^(-1/2)) + (1/2) * pi^4 * z^(-3/2) * (cos(pi*x)*sin(pi*y)*sin(2*pi*x)*cos(2*pi*y) + sin(pi*x)*cos(pi*y)*cos(2*pi*x)*sin(2*pi*y));
+    w = 2 * pi^2 * sin(pi*x) * sin(pi*y) * (2+z^(-1/2)) + (1/2) * pi^4 * z^(-3/2) * (cos(pi*x)*sin(pi*y)*sin(2*pi*x)*cos(2*pi*y) + sin(pi*x)*cos(pi*y)*cos(2*pi*x)*sin(2*pi*y));
 end
